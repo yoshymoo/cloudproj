474f9917c9f5a8af5f1c53befa91115af56ce4ed
Biyan Tay <biyantay@gmail.com>
2025-04-04 16:41:23 +0800

    Add backend Task model

--- amplify/backend/task/resource.ts
+++ amplify/backend/task/resource.ts
@@ -4,26 +4,11 @@ export const data = defineData({
   models: {
     Task: {
       fields: {
-        id: {
-          type: 'ID',
-          required: true
-        },
-        userID: {
-          type: 'String',
-          required: true
-        },
-        taskName: {
-          type: 'String',
-          required: true
-        },
-        isCompleted: {
-          type: 'Boolean',
-          required: true
-        },
-        timestamp: {
-          type: 'AWSDateTime',
-          required: true
-        }
+        id: { type: 'ID', required: true },
+        userID: { type: 'String', required: true },
+        taskName: { type: 'String', required: true },
+        isCompleted: { type: 'Boolean', required: true },
+        timestamp: { type: 'AWSDateTime', required: true }
       }
     }
   }
